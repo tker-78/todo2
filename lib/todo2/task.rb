b10613737fd278a461b51896a63970519632c617
takuya kinoshita <kktak02@gmail.com>
2022-05-10 06:40:55 +0900

    validations

--- lib/todo2/task.rb
+++ lib/todo2/task.rb
@@ -1,5 +1,6 @@
 module Todo2
   class Task < ActiveRecord::Base
+
     
     NOT_YET = 0
     DONE = 1
@@ -18,5 +19,10 @@ module Todo2
 
       # 通常のメソッド定義では、ActiveRecordRelationに接続できないため、scopeを用いて定義する
       scope :status_is, ->(status) { where(status: status )}
+
+    validates :name, presence: true, length: { maximum: 140 }
+    validates :content, presence: true
+    # validationでSTATUSを参照するため、STATUSの定義よりも下の行に記載する必要あり
+    validates :status, numericality: true, inclusion: {in: STATUS.values }
   end
 end
